ed883c674e3050f19af92e9a00b1fa3185cb1e2e
bnparham <parhambn.workmail@gmail.com>
2023-10-11 15:22:53 +0330

    add some of models to admin.py

--- core/api/admin.py
+++ core/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone_number', 'passport_number', 'license_plate']
+
+@admin.register(PhoneCalls)
+class PhoneCallsAdmin(admin.ModelAdmin):
+    list_display = ['caller', 'reciver', 'date', 'duration']
--- core/api/models.py
+++ core/api/models.py
@@ -1,33 +1,41 @@
 from django.db import models
 
 class Person(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     phone_number = models.IntegerField()
     passport_number = models.IntegerField()
-    license_plate = models.CharField()
+    license_plate = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'person'
+        verbose_name_plural = 'persons'
 
 class PhoneCalls(models.Model):
-    caller = models.ForeignKey(Person, on_delete=models.CASCADE)
-    reciver = models.ForeignKey(Person, on_delete=models.CASCADE)
+    caller = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='outgoing_calls')
+    reciver = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='incoming_calls')
     date = models.DateField()
     duration = models.IntegerField()
 
+    class Meta:
+        verbose_name = 'phone call'
+        verbose_name_plural = 'phone calls'
+
 class Location(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
 
 class Street(models.Model):
     location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
-    name = models.CharField()
+    name = models.CharField(max_length=200)
 
 class BankAccount(models.Model):
     account_number = models.IntegerField()
-    person_id = models.ForeignKey(Person)
+    person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
     created = models.DateField()
 
 class AtmTransaction(models.Model):
-    account_number = models.ForeignKey(BankAccount)
+    account_number = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
     atm_location = models.ForeignKey(Street, on_delete=models.CASCADE)
-    transaction_type = models.CharField()
+    transaction_type = models.CharField(max_length=200)
     amount = models.IntegerField()
     date = models.DateField()
 
@@ -35,54 +43,54 @@ class SecurityLog(models.Model):
     license_plate = models.ForeignKey(Person, on_delete=models.CASCADE)
     street_id = models.ForeignKey(Street, on_delete=models.CASCADE)
     location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
-    activity = models.CharField()
+    activity = models.CharField(max_length=200)
     date = models.DateField()
     time = models.TimeField()
 
 class CrimeSceneReport(models.Model):
     street_id = models.ForeignKey(Street, on_delete=models.CASCADE)
-    description = models.CharField()
+    description = models.CharField(max_length=200)
     date = models.DateField()
 
 class Item(models.Model):
-    name = models.CharField()
-    description = models.CharField()
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
 
 class Evidence(models.Model):
     crime_id = models.ForeignKey(CrimeSceneReport, on_delete=models.CASCADE)
     item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
 
 class Airports(models.Model):
-    full_name = models.CharField()
-    city = models.CharField()
+    full_name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
 
 class Flights(models.Model):
-    origin_airport_id = models.ForeignKey(Airports, on_delete=models.CASCADE)
-    destination_airport_id = models.ForeignKey(Airports, on_delete=models.CASCADE)
+    origin_airport_id = models.ForeignKey(Airports, on_delete=models.CASCADE, related_name='departing_flights')
+    destination_airport_id = models.ForeignKey(Airports, on_delete=models.CASCADE, related_name='arriving_flights')
     date = models.DateField()
     time = models.TimeField()
-    type = models.CharField()
+    type = models.CharField(max_length=200)
 
 class Passengers(models.Model):
     flight_id = models.ForeignKey(Flights, on_delete=models.CASCADE)
     person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
-    seat = models.CharField()
+    seat = models.CharField(max_length=200)
 
 class Interviews(models.Model):
-    name = models.CharField()
-    transcript = models.CharField()
+    name = models.CharField(max_length=200)
+    transcript = models.CharField(max_length=500)
     location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
     date = models.DateField()
 
 class MessageBox(models.Model):
     person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
-    body = models.CharField()
+    body = models.CharField(max_length=500)
     date = models.DateField()
 
 class player_reply(models.Model):
     message_id = models.ForeignKey(MessageBox, on_delete=models.CASCADE)
-    body = models.CharField()
+    body = models.CharField(max_length=500)
 
 class npc_reply(models.Model):
     player_reply_id = models.ForeignKey(player_reply, on_delete=models.CASCADE)
-    body = models.CharField()
+    body = models.CharField(max_length=500)
