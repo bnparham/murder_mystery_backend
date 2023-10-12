bbb2878744b31596d26a0b533afd479f498ca301
Kiarash Gerayeli <99744777+kiartante@users.noreply.github.com>
2023-10-12 18:09:36 +0330

    add models to admin

--- core/api/admin.py
+++ core/api/admin.py
@@ -1,14 +1,88 @@
 from django.contrib import admin
 from .models import *
+
+
 # Register your models here.
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ['name', 'phone_number', 'passport_number', 'license_plate']
 
+
 @admin.register(PhoneCalls)
 class PhoneCallsAdmin(admin.ModelAdmin):
     list_display = ['caller', 'reciver', 'date', 'duration']
 
+
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ['name']
+
+
+@admin.register(Street)
+class StreetAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(BankAccount)
+class BankaccountAdmin(admin.ModelAdmin):
+    list_display = ['account_number', 'person_id']
+
+
+@admin.register(AtmTransaction)
+class AtmTransactionAdmin(admin.ModelAdmin):
+    list_display = ['account_number', 'atm_location', 'amount']
+
+
+@admin.register(SecurityLog)
+class SecurityLogAdmin(admin.ModelAdmin):
+    list_display = ['license_plate', 'street_id']
+
+
+@admin.register(CrimeSceneReport)
+class CrimeSceneReportAdmin(admin.ModelAdmin):
+    list_display = ['steert_id']
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Evidence)
+class EvidenceAdmin(admin.ModelAdmin):
+    list_display = ['crime_id', 'item_id']
+
+
+@admin.register(Airports)
+class AirportsAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'city']
+
+
+@admin.register(Flights)
+class FlightsAdmin(admin.ModelAdmin):
+    list_display = ['origin_airport_id', 'destination_airport_id', 'date', 'time']
+
+
+@admin.register(Passengers)
+class PassengersAdmin(admin.ModelAdmin):
+    list_display = ['flight_id', 'person_id', 'seat']
+
+
+@admin.register(Interviews)
+class InterviewsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'location_id', ]
+
+
+@admin.register(MessageBox)
+class MessageBoxAdmin(admin.ModelAdmin):
+    list_display = ['person_id', 'body', 'date']
+
+
+@admin.register(player_reply)
+class player_replyAdmin(admin.ModelAdmin):
+    list_display = ['message_id', 'body']
+
+
+@admin.register(npc_reply)
+class npc_replyAdmin(admin.ModelAdmin):
+    list_display = ['player_reply_id', 'body']
--- core/api/models.py
+++ core/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Person(models.Model):
     name = models.CharField(max_length=200)
     phone_number = models.IntegerField()
@@ -9,6 +10,7 @@ class Person(models.Model):
     class Meta:
         verbose_name = 'person'
         verbose_name_plural = 'persons'
+
 
 class PhoneCalls(models.Model):
     caller = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='outgoing_calls')
@@ -20,6 +22,7 @@ class PhoneCalls(models.Model):
         verbose_name = 'phone call'
         verbose_name_plural = 'phone calls'
 
+
 class Location(models.Model):
     name = models.CharField(max_length=200)
 
@@ -27,14 +30,25 @@ class Location(models.Model):
         verbose_name = 'Location'
         verbose_name_plural = "Locations"
 
+
 class Street(models.Model):
     location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Street'
+        verbose_name_plural = 'Streets'
+
 
 class BankAccount(models.Model):
     account_number = models.IntegerField()
     person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
     created = models.DateField()
+
+    class Meta:
+        verbose_name = 'Bank Account'
+        verbose_name_plural = 'Bank Accounts'
+
 
 class AtmTransaction(models.Model):
     account_number = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
@@ -42,6 +56,11 @@ class AtmTransaction(models.Model):
     transaction_type = models.CharField(max_length=200)
     amount = models.IntegerField()
     date = models.DateField()
+
+    class Meta:
+        verbose_name = 'Atm Transaction'
+        verbose_name_plural = 'Atm Transactions'
+
 
 class SecurityLog(models.Model):
     license_plate = models.ForeignKey(Person, on_delete=models.CASCADE)
@@ -51,22 +70,47 @@ class SecurityLog(models.Model):
     date = models.DateField()
     time = models.TimeField()
 
+    class Meta:
+        verbose_name = 'Security Log'
+        verbose_name_plural = 'Security Logs'
+
+
 class CrimeSceneReport(models.Model):
     street_id = models.ForeignKey(Street, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
     date = models.DateField()
 
+    class Meta:
+        verbose_name = 'Crime Scene Report'
+        verbose_name_plural = 'Crime Scene Reports'
+
+
 class Item(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
+
+    class Meta:
+        verbose_name = 'Item'
+        verbose_name_plural = 'Items'
+
 
 class Evidence(models.Model):
     crime_id = models.ForeignKey(CrimeSceneReport, on_delete=models.CASCADE)
     item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = 'Evidence'
+        verbose_name_plural = 'Evidences'
+
+
 class Airports(models.Model):
     full_name = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Airport'
+        verbose_name_plural = 'Airports'
+
 
 class Flights(models.Model):
     origin_airport_id = models.ForeignKey(Airports, on_delete=models.CASCADE, related_name='departing_flights')
@@ -75,10 +119,20 @@ class Flights(models.Model):
     time = models.TimeField()
     type = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = 'Flight'
+        verbose_name_plural = 'Flights'
+
+
 class Passengers(models.Model):
     flight_id = models.ForeignKey(Flights, on_delete=models.CASCADE)
     person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
     seat = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Passenger'
+        verbose_name_plural = 'Passengers'
+
 
 class Interviews(models.Model):
     name = models.CharField(max_length=200)
@@ -86,15 +140,34 @@ class Interviews(models.Model):
     location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
     date = models.DateField()
 
+    class Meta:
+        verbose_name = 'Interview'
+        verbose_name_plural = 'Interviews'
+
+
 class MessageBox(models.Model):
     person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
     body = models.CharField(max_length=500)
     date = models.DateField()
 
+    class Meta:
+        verbose_name = 'Message Box'
+        verbose_name_plural = 'Message Boxes'
+
+
 class player_reply(models.Model):
     message_id = models.ForeignKey(MessageBox, on_delete=models.CASCADE)
     body = models.CharField(max_length=500)
 
+    class Meta:
+        verbose_name = 'Player Reply'
+        verbose_name_plural = 'Player Replies'
+
+
 class npc_reply(models.Model):
     player_reply_id = models.ForeignKey(player_reply, on_delete=models.CASCADE)
     body = models.CharField(max_length=500)
+
+    class Meta:
+        verbose_name = 'Npc Reply'
+        verbose_name_plural = 'npc Replies'
